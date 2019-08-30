15548f0ac86e12fe77543e4bd83bd24d1650c77c
suzhiwei <suzhiwei@ybm100.com>
2019-08-30 17:02:31 +0800

    删除迁移分支时，定时任务，以及其他无用说明

--- src/main/java/com/szw/springbootdemosu/controller/HelloController.java
+++ src/main/java/com/szw/springbootdemosu/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.szw.springbootdemosu.controller;
 
-import com.szw.springbootdemosu.config.Person;
 import com.szw.springbootdemosu.service.SaasPurchaseBillDetialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @Autowired
-    Person person;
-    @Autowired
     SaasPurchaseBillDetialService saasPurchaseBillDetialService;
     @RequestMapping("/sayHello")
     public String hello(){
-        System.out.println(person);
         return "hello world - "+saasPurchaseBillDetialService.querySaasPurchaseBillDetialIp(2);
     }
 }
--- src/test/java/com/szw/springbootdemosu/SpringbootdemosuApplicationTests.java
+++ src/test/java/com/szw/springbootdemosu/SpringbootdemosuApplicationTests.java
@@ -1,6 +1,5 @@
 package com.szw.springbootdemosu;
 
-import com.szw.springbootdemosu.config.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
