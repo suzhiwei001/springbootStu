c7f281014f07ec4f8fabac07393e998aa1220ebf
suzhiwei <suzhiwei@ybm100.com>
2019-09-05 10:50:02 +0800

    删除迁移分支时，定时任务，以及其他无用说明

---
+++ src/main/java/com/szw/springbootdemosu/service/AsyncService.java
@@ -0,0 +1,13 @@
+package com.szw.springbootdemosu.service;
+
+/**
+ * @author suzhiwei
+ * @Date 2019/9/3
+ * @Describe
+ */
+public interface AsyncService {
+    /**
+     * 执行异步任务
+     */
+    void executeAsync();
+}
