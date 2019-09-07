8495cbb38cc73573241d2d4d48c568be2bf0b8bb
suzhiwei <suzhiwei@ybm100.com>
2019-09-07 15:26:20 +0800

    mybatis-plus集成测试

--- src/main/java/com/szw/springbootdemosu/controller/HelloController.java
+++ src/main/java/com/szw/springbootdemosu/controller/HelloController.java
@@ -1,16 +1,12 @@
 package com.szw.springbootdemosu.controller;
 
-import com.szw.springbootdemosu.service.SaasPurchaseBillDetialService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @Autowired
-    SaasPurchaseBillDetialService saasPurchaseBillDetialService;
     @RequestMapping("/sayHello")
     public String hello(){
-        return "hello world - "+saasPurchaseBillDetialService.querySaasPurchaseBillDetialIp(2);
+        return "hello world";
     }
 }
