2231b0f01bdbe998d2234e9fe7ae8a43eb1c8c21
suzhiwei <suzhiwei@ybm100.com>
2019-10-22 18:08:52 +0800

    集成jedis

--- src/test/java/com/szw/springbootdemosu/SpringbootdemosuApplicationTests.java
+++ src/test/java/com/szw/springbootdemosu/SpringbootdemosuApplicationTests.java
@@ -2,14 +2,32 @@ package com.szw.springbootdemosu;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootdemosuApplicationTests {
+    @Autowired
+    private JedisPool jedisPool;
     @Test
     public void contextLoads() {
 
+    }
+
+    @Test
+    public void set() {
+        Jedis jedis = jedisPool.getResource();
+        jedis.set("abc", "123");
+    }
+
+    @Test
+    public void get() {
+        Jedis jedis = jedisPool.getResource();
+        String value = jedis.get("abc");
+        System.out.println(value);
     }
 }
